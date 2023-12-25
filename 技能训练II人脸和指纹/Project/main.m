f7d5c8cf25a83dcbfea6eb09a9f1fd80c6ff800e
Cossiant <L861149396@163.COM>
2023-12-25 14:06:28 +0800

    添加Python的opencv相关
    现在可以使用python的opencv相关代码了，甚至可以使用.py后缀的代码了，进入python时代

--- 技能训练II人脸和指纹/Project/main.m
+++ 技能训练II人脸和指纹/Project/main.m
@@ -1,3 +1,13 @@
-res1 = py.int(3);
-res2 = py.int(3);
-res = res1+res2
+clc;
+clear;
+close all;
+%% 使用前请调用一次
+% pyversion(".\Python308\python.exe");
+% 这是相关使用教程
+% https://blog.csdn.net/wenhao_ir/article/details/124888473
+
+img = py.cv2.imread(".\Image\face\test\113676980_p0.png");
+
+py.cv2.imshow("img",img);
+
+py.cv2.waitKey();
